23c5735ce5f954cc136c54a10aff437ad7e1b85b
Don Bernhardy <donbernhardy@gmail.com>
2020-07-14 16:24:43 -0500

    Upgrade dotnet core 3.1

--- PPOLMorningChecklist/PPOLMorningChecklistTests.cs
+++ PPOLMorningChecklist/PPOLMorningChecklistTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Comdata.AppSupport.PPOLMorningChecklist
+﻿namespace Comdata.AppSupport.PPOLMorningChecklist
 {
     [TestClass]
     public class PPOLMorningChecklistTests
